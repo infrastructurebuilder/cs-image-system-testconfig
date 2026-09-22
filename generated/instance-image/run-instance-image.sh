a2d3af177385ad2bdfb7bd3bf00976bc71ea5829
Mykel Alvis <104666+mykelalvis@users.noreply.github.com>
2026-09-21 22:24:29 -0500

    cs-image-system run 2026_09_21t22_21_25_452025: instance-image
    Meta-state commit: read-models, pins, lineage, launch parameters and generated IaC for lifecycles [instance-image].
    Run id: 2026_09_21t22_21_25_452025

--- generated/instance-image/run-instance-image.sh
+++ generated/instance-image/run-instance-image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # cs-image-system lifecycle runner: instance-image
-# run id: 2026_09_21t22_18_06_561584
+# run id: 2026_09_21t22_21_25_452025
 # Deferred commands accumulated while generating this lifecycle,
 # in phase order. Paths are relative to this lifecycle's directory.
 # state: workspace open-tofu -> s3://noaa-ioos-cloud-sandbox-tfstate/statefiles/csia-image-system-test/open_tofu.tfstate
