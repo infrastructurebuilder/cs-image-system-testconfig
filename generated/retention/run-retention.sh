6017098704b811c759c5efab64b1fde024c9de16
Mykel Alvis <104666+mykelalvis@users.noreply.github.com>
2026-09-25 15:37:36 -0500

    cs-image-system run 2026_09_25t15_16_23_826583: identity, storage, base-image, instance-image, release, retention
    Meta-state commit: read-models, pins, lineage, launch parameters and generated IaC for lifecycles [identity, storage, base-image, instance-image, release, retention].
    Run id: 2026_09_25t15_16_23_826583

--- generated/retention/run-retention.sh
+++ generated/retention/run-retention.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # cs-image-system lifecycle runner: retention
-# run id: 2026_09_25t11_28_31_278610
+# run id: 2026_09_25t15_16_23_826583
 # Deferred commands accumulated while generating this lifecycle,
 # in phase order. Paths are relative to this lifecycle's directory.
 # NOTE: builders' pre/post finalize hooks are NOT part of this
